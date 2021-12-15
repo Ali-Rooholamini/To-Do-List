55301c250e6cf85e16c0dd1299aba7d9a3e9178b
Ali Rooholamini <ali.rooholamini.dev@gmail.com>
2021-12-15 17:29:01 +0330

    add to list actived , including (enter todo Title)error and fixed main page design

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Switch between Dark/Light Mode
 const nightMode = document.getElementById("nightmode");
 const checkMode = document.getElementById("darkmode");
 const nightModeIcon = document.querySelector(".title label span i");
@@ -13,5 +14,40 @@ function changeMode(){
         checkMode.setAttribute("checked" , "");
         nightMode.setAttribute("href" , "nightmode.css");
         nightModeIcon.classList.replace("fa-moon","fa-sun");
+    }
+}
+
+// add too list machine
+const listFooter = document.querySelector("div.insert.list footer");
+const newTodo = document.querySelector("div.insert input");
+const notify = document.getElementById("notify");
+let notifyFlag = true;
+
+newTodo.addEventListener("keypress" , function(event){
+    if(event.keyCode === 13){
+        event.preventDefault();
+        addTodo();
+    }
+});
+
+function addTodo() {
+    if(newTodo.value){
+        let labelTag = document.createElement("label");
+        let listElements = `<input type=checkbox><div class=checkbox><i class='fas fa-check'></i></div><input type=text value='${newTodo.value}' disabled><div class=tools><button><i class='fas fa-pen'></i></button><button><i class='fas fa-times'></i></button></div>`;
+        labelTag.innerHTML = listElements;
+        listFooter.before(labelTag);
+        newTodo.value = "";
+    }else{
+        if(notifyFlag){
+            notifyFlag = false;
+            notify.classList.add("notifying");
+            setTimeout(function(){
+                notify.classList.add("notified");
+            }, 3200);
+            setTimeout(function(){
+                notify.classList.remove("notified" , "notifying");
+                notifyFlag = true;
+            } , 4200);
+        }
     }
 }
